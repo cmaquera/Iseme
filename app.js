ecaae99aa7f398478433f222234017e18b1d9b8b
Cesar Maquera <yushiroche@gmail.com>
2017-05-17 21:13:47 -0500

    Comiezo

---
+++ app.js
@@ -0,0 +1,112 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('static-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var cfenv = require('cfenv');
+var http = require('http');
+var path = require('path');
+var cv =require('opencv');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+/// catch 404 and forwarding to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);  
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+var debug = require('debug')('Iseme');
+
+app.set('port', process.env.PORT || 3000);
+
+server.listen(app.get('port'), function(){
+    console.log('Serving on port ' + app.get('port'));
+});
+
+
+// camera properties
+var camWidth = 320;
+var camHeight = 240;
+var camFps = 120;
+var camInterval = 1000 / camFps;
+
+// face detection properties
+var rectColor = [0, 255, 0];
+var rectThickness = 2;
+
+// initialize camera
+var camera = new cv.VideoCapture(0);
+camera.setWidth(camWidth);
+camera.setHeight(camHeight);
+
+
+// WebSocket server
+
+
+io.on('connection', function (socket) {
+  setInterval(function() {
+    camera.read(function(err, im) {
+      if (err) throw err;
+
+      im.detectObject('./node_modules/opencv/data/haarcascade_frontalface_alt2.xml', {}, function(err, faces){
+       if (err) throw err;
+
+       for (var i = 0; i < faces.length; i++){
+         var face = faces[i];
+         im.ellipse(face.x + face.width / 2, face.y + face.height / 2, face.width / 2, face.height / 2, [255, 255, 0], 3);
+       }
+       socket.emit('frame', { buffer: im.toBuffer() });
+      }); 
+    });
+  }, 700);
+});
+
+
+
+
